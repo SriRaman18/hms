00cc20965fc92b1c3d9dcf76a11edea13e646a78
SriRaman18 <sriraman434@gmail.com>
2023-11-23 15:49:27 +0530

    assets change

---
+++ src/assets/index.tsx
@@ -0,0 +1,39 @@
+import { ReactComponent as Accounts } from "./Accounts.svg";
+import { ReactComponent as Admin } from "./Admin.svg";
+import { ReactComponent as AppointMents } from "./AppointMents.svg";
+import { ReactComponent as AppointmentPic } from "./AppointmentPic.svg";
+import { ReactComponent as Avatar } from "./Avatar.svg";
+import { ReactComponent as Billing } from "./Billing.svg";
+import { ReactComponent as DashBoard } from "./DashBoard.svg";
+import { ReactComponent as Inpatients } from "./Inpatients.svg";
+import { ReactComponent as Laboratory } from "./Laboratory.svg";
+import { ReactComponent as Logo } from "./Logo.svg";
+import { ReactComponent as Menu } from "./Menu.svg";
+import { ReactComponent as Patients } from "./Patients.svg";
+import { ReactComponent as Payment } from "./Payment.svg";
+import { ReactComponent as Pharmacy } from "./Pharmacy.svg";
+import { ReactComponent as Procurements } from "./Procurements.svg";
+import { ReactComponent as Reports } from "./Reports.svg";
+import { ReactComponent as StentsRegister } from "./StentsRegister.svg";
+import { ReactComponent as Vaccines } from "./Vaccines.svg";
+
+export {
+  Accounts,
+  Admin,
+  AppointMents,
+  AppointmentPic,
+  Avatar,
+  Billing,
+  DashBoard,
+  Inpatients,
+  Laboratory,
+  Logo,
+  Menu,
+  Patients,
+  Payment,
+  Pharmacy,
+  Procurements,
+  Reports,
+  StentsRegister,
+  Vaccines,
+};
